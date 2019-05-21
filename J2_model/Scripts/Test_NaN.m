cb5fc6da5b8ba234c60debad738c54cbf0eeaf54
jtdorsey22 <jonathandorsey1117@gmail.com>
2019-05-20 19:33:46 -0700

    Update

--- J2_model/Scripts/Test_NaN.m
+++ J2_model/Scripts/Test_NaN.m
@@ -3,16 +3,11 @@ thing = isnan(V);
 list  =0 ; 
 counter = 1; 
 
-for k =1:length(t)
-    
+for k =1:length(t)    
     if thing(k) == 1
        
-        list(counter) = k; 
-        
-        counter = counter +1;    
-        
+        list(counter) = k;         
+        counter = counter +1;        
          
-    end 
-    
-    
+    end    
 end 
