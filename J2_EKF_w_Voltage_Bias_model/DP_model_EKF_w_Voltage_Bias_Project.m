7e8ba6a64b3d4eace62fa07756dd1e3c6dba18e3
jtdorsey22 <jonathandorsey1117@gmail.com>
2019-05-22 19:05:17 -0700

    Rename J2 EKF Project, add EKF with bias

--- J2_EKF_w_Voltage_Bias_model/DP_model_EKF_w_Voltage_Bias_Project.m
+++ J2_EKF_w_Voltage_Bias_model/DP_model_EKF_w_Voltage_Bias_Project.m
@@ -14,11 +14,11 @@ KalmanParams
 % Load Battery Measurements 
 load('OCV_table.mat')
 load('OCV_slope_table.mat')
-load('IV_data_nonlinear.mat')
-% load('SimTruth1.mat')
+% load('IV_data_nonlinear.mat')
+load('Sim_Truth_FifthOrder.mat')
 
-% R= 400000; 
-% Q = 10;
+Q= 1; 
+R = 1000;
 
 %% Observability & Controlability Analysis: 
 
@@ -52,6 +52,7 @@ for k = 2:1:length(t)
     
     % Model Prediction: 
     x1_hat_prev = Ad(1,1)*x1_hat(k-1) + Bd(1,1)*I(k-1);
+    
     C_ek = interp1(soc_intpts_OCV_slope', OCV_slope_intpts, x1_hat_prev);
 
     P_prev = A_ek*P(k-1)*A_ek'+ E_ek*Q*E_ek';
@@ -97,52 +98,57 @@ xlabel('Error')
 ylabel('Prob. Density');
 legend('SOC Error','~N(0,P)'), title('SOC Error Distribution (Jonathan Dorsey)');
 
-%% Simulated Truth : 
-clear all, clc
-% Import Battery Parameters 
-BatteryParams    
 
-Voc0 = 3.435;
-% Import Kalman Parameters
-KalmanParams
 
-% Load Battery Measurements 
-load('OCV_table.mat')
-load('OCV_slope_table.mat')
-load('IV_data_nonlinear.mat')
+%% RMS Error Optimation: 
 
+R_list = [.1,1,10,100,1000,2000,5000,10000]; 
+
+
+for l = 1:length(R_list)
+    
+    Q= 1; 
+R = R_list(l);
+
+% Initial Conditions: 
 P(1) = 0;           % Covariance 
 x1(1) = 1;          % SOC - Battery Fully Charged 
 x2(1) = 0;          % Vc1
 x3(1) = 0;          % Vc2
 
-x1_hat(1) = x1(1); 
-
-var = 3.5*10^-7;
-% var = 2.5*10^-6;
-
-% var = 0;
+x1_hat(1) = 1; 
 
 for k = 2:1:length(t)
     
-    x1(k) = Ad(1,1)*x1(k-1) + Bd(1,1)*I(k-1)+normrnd(0,sqrt(var)); % soc
-    x2(k) = Ad(2,2)*x2(k-1) + Bd(2,1)*I(k-1) +normrnd(0,sqrt(var)); % Vc1
-    x3(k) = Ad(3,3)*x3(k-1) + Bd(3,1)*I(k-1)+ normrnd(0,sqrt(var)); % Vc2
+    x1(k) = Ad(1,1)*x1(k-1) + Bd(1,1)*I(k-1); % soc
+    x2(k) = Ad(2,2)*x2(k-1) + Bd(2,1)*I(k-1); % Vc1
+    x3(k) = Ad(3,3)*x3(k-1) + Bd(3,1)*I(k-1); % Vc2
     
-     V_truth(k) = interp1(soc_intpts_OCV',OCV_intpts,x1(k-1)) - I(k-1)*R0 - x2(k-1)- x3(k-1)+normrnd(0,sqrt(R));
+    
+    % Model Prediction: 
+    x1_hat_prev = Ad(1,1)*x1_hat(k-1) + Bd(1,1)*I(k-1);
+    
+    C_ek = interp1(soc_intpts_OCV_slope', OCV_slope_intpts, x1_hat_prev);
 
+    P_prev = A_ek*P(k-1)*A_ek'+ E_ek*Q*E_ek';
+    
+   % Measurement Update: 
+   V_hat(k) = interp1(soc_intpts_OCV',OCV_intpts,x1_hat_prev) - I(k-1)*R0 - x2(k-1) - x3(k-1);
+   % V_hat(k) = Voc0 + alpha*x1_hat_prev - I(k-1)*R0 - x2(k-1)- x3(k-1);
+    
+L = P_prev*C_ek'*inv(C_ek*P_prev*C_ek'+ F_ek*R*F_ek');
+    
+    x1_hat(k) = x1_hat_prev + L*(V(k)-V_hat(k));
+    P(k) = P_prev - L*C_ek*P_prev;
+    
 end 
 
-
-
-figure()
-hold on 
-plot(t,x1)
-plot(t,SOC_act)
-legend('Simulated Truth','Lin_SOC_act');
-
-figure(); 
-plot(t,V_truth)
+    
+error_rms(l) = sqrt(mean((SOC_act-x1_hat).^2)); 
+    
+    
+    
+end 
 
 
 
